c5be39d882832d1c504f93f13d386bb845c56380
M. Audrey Chapda <tchapsdev@gmail.com>
2025-02-12 05:31:36 -0500

    initial curd

---
+++ WebAppMVC/Controllers/UsersController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppMVC.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Users";
+
+            ViewBag.Country = "CA";
+
+            return View();
+        }
+    }
+}
---
+++ WebAppMVC/Models/Customer.cs
@@ -0,0 +1,25 @@
+﻿namespace WebAppMVC.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+
+        public string Region { get; set; }
+
+        public string PostalCode { get; set; }
+
+        public string Country { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Fax { get; set; }
+    }
+}
